eba3b940a52d779381ca2a747b8931560f4962a6
darodelo <dariodelolmo@gmail.com>
2022-10-26 10:40:20 -0300

    Actualizado el banner de trademarks

--- src/Components/TrademarksBanner.jsx
+++ src/Components/TrademarksBanner.jsx
@@ -8,19 +8,19 @@ const TrademarksBanner = () => {
     <div className='trademarks-banner'>
         
         <div className='trademarks-img-banner'>
-            <img src="./disney-logo.svg" alt="Disney logo" height="75px" />
+            <a href="#"> <img src="./disney-logo.svg" alt="Disney logo" height="75px" /> </a>
         </div>
 
         <div className='trademarks-img-banner'>
-            <img src="./marvel-logo.png" alt="Marvel logo" height="75px" />
+            <a href="#"> <img src="./marvel-logo.png" alt="Marvel logo" height="75px" /> </a>
         </div>
         
         <div className='trademarks-img-banner'>
-            <img src="./star-wars-logo.png" alt="Star Wars logo" height="75px" />
+             <a href="#"> <img src="./star-wars-logo.png" alt="Star Wars logo" height="75px" /> </a>
         </div>
 
         <div className='trademarks-img-banner'>
-            <img src="./harry-potter-logo.png" alt="Harry Potter logo" height="75px" />
+            <a href="#"> <img src="./harry-potter-logo.png" alt="Harry Potter logo" height="75px" /> </a>
         </div>
 
     </div>
